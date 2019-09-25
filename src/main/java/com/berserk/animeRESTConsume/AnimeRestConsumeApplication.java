b79db008c5353a5ea6cb55ee7210206b9721f9c9
berserkV <valyria.song@gmail.com>
2019-09-25 11:12:23 -0500

    Init project

---
+++ src/main/java/com/berserk/animeRESTConsume/AnimeRestConsumeApplication.java
@@ -0,0 +1,13 @@
+package com.berserk.animeRESTConsume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimeRestConsumeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimeRestConsumeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/berserk/animeRESTConsume/AnimeRestConsumeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.berserk.animeRESTConsume;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AnimeRestConsumeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
