60a01007500fdf20a4a3da9e447b40fb5466ff5f
berserkV <valyria.song@gmail.com>
2019-09-26 09:44:47 -0500

    Added successful unit tests for service's methods

--- src/test/java/com/berserk/animeRESTConsume/service/AnimeServiceTest.java
+++ src/test/java/com/berserk/animeRESTConsume/service/AnimeServiceTest.java
@@ -1,11 +1,13 @@
 package com.berserk.animeRESTConsume.service;
 
 import static org.junit.Assert.assertTrue;
-import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,26 +15,16 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import com.berserk.animeRESTConsume.model.Anime;
-import com.berserk.animeRESTConsume.repository.AnimeRepository;
-import com.berserk.animeRESTConsume.service.implementation.AnimeServiceImplementation;
 
 @RunWith(MockitoJUnitRunner.class)
 public class AnimeServiceTest {
 
 	@Mock
-	AnimeRepository repository;
-	@Mock
-	AnimeService service = new AnimeServiceImplementation();
-
-//	@Before
-//	public void setup() {
-//		MockitoAnnotations.initMocks(this);
-//	}
+	AnimeService service;
 
 	@Test
 	public void whenProcessJson_thenReturnTrue() {
 		//given
-		when(repository.saveAll(new ArrayList<Anime>()).thenReturn(new ArrayList<Anime>());
 		when(service.processJson(anyString())).thenReturn(true);
 
 		//when
@@ -41,5 +33,27 @@ public class AnimeServiceTest {
 		//then
 		assertTrue(result);
 	}
+	
+	@Test
+	public void givenAnimeList_whenSave_thenReturnTrue() {
+		//given
+		List<Anime> myAnimeList = new ArrayList<>(
+				Arrays.asList(new Anime(
+						1L, 
+						null, 
+						"TV", 
+						"One piece", 
+						null, 
+						null, 
+						null, 
+						958, 
+						new ArrayList<>())));
 
+		//when
+		doReturn(true).when(service).save(myAnimeList);
+
+		//then
+		assertTrue(service.save(myAnimeList));
+		
+	}
 }
